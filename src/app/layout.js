9682af5551287ee9fbef04d88deca64b297c12ca
Gabo Duran <gduran.ex@meatcode.cl>
2024-09-19 03:50:41 -0400

    feat: footer add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import localFont from "next/font/local";
 import "../styles/global.scss";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
